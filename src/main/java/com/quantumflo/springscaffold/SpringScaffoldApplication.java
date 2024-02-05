b5f0bad7749c66d426d019fd2427cdf9ac6a5653
shashank <shashankhebbarm@gmail.com>
2024-02-05 10:40:39 +0530

    init

---
+++ src/main/java/com/quantumflo/springscaffold/SpringScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.quantumflo.springscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantumflo/springscaffold/SpringScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumflo.springscaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
